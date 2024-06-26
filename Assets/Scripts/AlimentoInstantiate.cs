dd1cf951b02c06f730f667791d1b13caeae1fd43
SantiGamer41 <dalessanti41@gmail.com>
2024-06-26 10:12:17 -0300

    primera etapa array comida

---
+++ Assets/Scripts/AlimentoInstantiate.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlimentoInstantiate : MonoBehaviour
+{
+    public int index = -1;
+    public GameObject[] comida;
+    public GameObject[] comidasEnPlano;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        deactivateAll();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void deactivateAll()
+    {
+        for (int i = 0; i <= comida.Length - 1; i++)
+        {
+            comida[i].SetActive(false);
+        }
+    }
+    void aparecerComida()
+    {
+        for (int i = Random.Range(0, comida.Length - 1); i < comida.Length - 1; i = Random.Range(0, comida.Length - 1))
+        {
+            // comida[i]
+        }
+    }
+}
